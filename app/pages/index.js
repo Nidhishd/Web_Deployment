fc69d4e0408b130f1821a6286fff1137db4c4b27
Nidhishd <56064558+Nidhishd@users.noreply.github.com>
2020-05-03 20:19:22 +0530

    Update index.js

--- app/pages/index.js
+++ app/pages/index.js
@@ -7,7 +7,7 @@ var http = require('http');
 http.createServer(function (req, res)
 {
     var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-    if (ip == '43.229.90.48') // exit if it's a particular ip
+    if (ip == '40.xx.xx.xx') // exit if it's a particular ip
         res.end();
 }
                   )
